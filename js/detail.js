e443b8ba1120417c6bfb38a01031760fbfed790b
winterjie <124400122@qq.com>
2014-08-03 13:45:50 +0800

    add course and detail

--- js/detail.js
+++ js/detail.js
@@ -1,8 +1,26 @@
-define(function(require) {
 
-	var $ = require('jquery');
-  	require('jquery.easing');
+/**
+ *  导航栏的鼠标移动响应
 
 
+$(function(){
+
+    var navi_li=$(".navi_ul li");
+    navi_li.mouseenter(function(){
+        navi_li.css("background-color","#fff");
+    });
+    navi_li.mouseleave(function(){
+        this.css("background-color","#fafafa");
+    })
 });
+ */
 
+/** 回到顶端 */
+
+$(function(){
+    var btn=$('.course_detail_tool_bar div').eq(2);
+    btn.click(function(){
+        $("body,html").animate({scrollTop:0},500)
+
+    })
+});
