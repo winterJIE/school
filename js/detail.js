2cf56ebd78c2231f983e340385cbb7615ee52e50
ChenCody <cookie605719538@gmail.com>
2014-08-07 23:51:01 +0800

    slide bar in index/education

--- js/detail.js
+++ js/detail.js
@@ -1,19 +1,20 @@
-
-/**
- *  导航栏的鼠标移动响应
-
+//主页  “专业教育栏滑动”
 
 $(function(){
-
-    var navi_li=$(".navi_ul li");
-    navi_li.mouseenter(function(){
-        navi_li.css("background-color","#fff");
+    var list=$('.subj_list');
+    var list_child=$('.subj_list li');
+    var box_width=$('.index_subj_box').width();
+    var li_width=list_child.eq(0).width();
+    var show_num=Math.ceil(box_width/li_width);
+    var li_total_length=list_child.length*li_width-box_width;
+    var move_length=
+    list_child.eq(show_num-1).mouseenter(function(){
+        list.animate({right:li_total_length},300)
+        });
+    list_child.eq(0).mouseenter(function(){
+        list.animate({right:0},300)
     });
-    navi_li.mouseleave(function(){
-        this.css("background-color","#fafafa");
-    })
 });
- */
 
 /** 回到顶端 */
 
