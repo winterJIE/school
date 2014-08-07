aa409dd31c11bd38edd6f6950794c9d1663a99be
ChenCody <cookie605719538@gmail.com>
2014-08-07 23:59:15 +0800

    slide bar in index/education

--- js/detail.js
+++ js/detail.js
@@ -6,10 +6,9 @@ $(function(){
     var box_width=$('.index_subj_box').width();
     var li_width=list_child.eq(0).width();
     var show_num=Math.ceil(box_width/li_width);
-    var li_total_length=list_child.length*li_width-box_width;
-    var move_length=
+    var move_length=list_child.length*li_width-box_width;
     list_child.eq(show_num-1).mouseenter(function(){
-        list.animate({right:li_total_length},300)
+        list.animate({right:move_length},300)
         });
     list_child.eq(0).mouseenter(function(){
         list.animate({right:0},300)
