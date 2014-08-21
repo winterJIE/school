84b8d1551e6bc247231ff8ae75cc2b428a1870eb
ChenCody <cookie605719538@gmail.com>
2014-08-21 20:32:16 +0800

    back_end managing system(completed)

--- js/back_end.js
+++ js/back_end.js
@@ -1,19 +1,19 @@
 $(function(){
     var menu_list=$('.back_end_menu>li');
     menu_list.eq(0).click(function(){
-        $('.back_end_tab .back_end_index').show(300).sibling().hidden(300);
-    })
+        $('.back_end_tab .back_end_index').show(300).siblings().hide(300);
+    });
     menu_list.eq(1).click(function(){
-        $('.back_end_tab .about_us').show(300).sibling().hidden(300);
-    })
+        $('.back_end_tab .about_us').show(300).siblings().hide(300);
+    });
     menu_list.eq(3).click(function(){
-        $('.back_end_tab .yipin_course').show(300).sibling().hidden(300);
-    })
+        $('.back_end_tab .yipin_course').show(300).siblings().hide(300);
+    });
     menu_list.eq(5).click(function(){
-        $('.back_end_tab .back_yipin_info').show(300).sibling().hidden(300);
-    })
+        $('.back_end_tab .back_yipin_info').show(300).siblings().hide(300);
+    });
     menu_list.eq(6).click(function(){
-        $('.back_end_tab .managing_member').show(300).sibling().hidden(300);
-    })
+        $('.back_end_tab .managing_member').show(300).siblings().hide(300);
+    });
 
 });
