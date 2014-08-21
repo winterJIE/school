eebd39557c0db654c2a5add999f9e0c7f4d6e5d5
ChenCody <cookie605719538@gmail.com>
2014-08-22 00:53:23 +0800

    back_end managing system(completed)

--- js/back_end.js
+++ js/back_end.js
@@ -1,19 +1,22 @@
-$(function(){
-    var menu_list=$('.back_end_menu>li');
-    menu_list.eq(0).click(function(){
-        $('.back_end_tab .back_end_index').show(300).siblings().hide(300);
-    });
-    menu_list.eq(1).click(function(){
-        $('.back_end_tab .about_us').show(300).siblings().hide(300);
-    });
-    menu_list.eq(3).click(function(){
-        $('.back_end_tab .yipin_course').show(300).siblings().hide(300);
-    });
-    menu_list.eq(5).click(function(){
-        $('.back_end_tab .back_yipin_info').show(300).siblings().hide(300);
-    });
-    menu_list.eq(6).click(function(){
-        $('.back_end_tab .managing_member').show(300).siblings().hide(300);
-    });
+$(function () {
+    function tab_switch(index, selector) {
+        menu_list.eq(index).click(function () {
+            selector.show(300).siblings().hide(300);
+            menu_list.eq(index).css('color','#e30113').siblings().css('color','#4c4c4c');
+        })
+    }
+
+    var menu_list = $('.back_end_menu>li');
+
+    tab_switch(0,$('.back_end_index'));
+    tab_switch(1,$('.about_us'));
+    tab_switch(3,$('.yipin_course'));
+    tab_switch(5,$('.back_yipin_info'));
+    tab_switch(6,$('.managing_member'));
+
+    $('.back_menu_secondary').eq(2).click(function () {
+        $('.outstanding').show(300).parents().hide(300);
+        $('.back_menu_secondary').eq(2).css('color','#e30113').parents().css('color','#4c4c4c');
+    })
 
 });
